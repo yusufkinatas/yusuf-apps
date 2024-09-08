6753c4f0c32f585558421fd815b8a74f64f743be
Yusuf Kinatas <yusufkinatas@gmail.com>
2024-09-08 07:55:11 +0300

    add error boundary

---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,40 @@
+import { PropsWithChildren, useEffect, useState } from 'react'
+
+export const ErrorBoundary = ({ children }: PropsWithChildren) => {
+  const [errorMessage, setErrorMessage] = useState('')
+
+  useEffect(() => {
+    const handleError = (event: PromiseRejectionEvent | ErrorEvent) => {
+      let message = ''
+
+      if (event instanceof PromiseRejectionEvent) {
+        message =
+          event.reason instanceof Error ? event.reason.message : String(event.reason)
+      } else {
+        message = event.message
+      }
+
+      setErrorMessage(message)
+      console.error('ErrorBoundary caught an error:', message)
+    }
+
+    window.addEventListener('error', handleError)
+    window.addEventListener('unhandledrejection', handleError)
+
+    return () => {
+      window.removeEventListener('error', handleError)
+      window.removeEventListener('unhandledrejection', handleError)
+    }
+  }, [])
+
+  if (errorMessage) {
+    return (
+      <div className="min-h-lvh bg-red-400 text-white p-4 text-xl">
+        <b>Something went wrong</b>
+        <p>{errorMessage}</p>
+      </div>
+    )
+  }
+
+  return children
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './app'
+import { ErrorBoundary } from './components/error-boundary'
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 )
