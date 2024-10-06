7614a5852141cacf8340005ba70fa3c9426730a5
Yusuf Kinatas <yusufkinatas@gmail.com>
2024-10-06 22:57:18 +0300

    add global styles for body

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ErrorBoundary } from 'react-error-boundary'
-import { ThemeUIProvider } from 'theme-ui'
+import { Global, ThemeUIProvider } from 'theme-ui'
 
 import { App } from './app'
 import { AppError } from './components/app-error'
@@ -11,6 +11,7 @@ ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
     <ThemeUIProvider theme={theme}>
       <ErrorBoundary FallbackComponent={AppError}>
+        <Global styles={{ body: { overscrollBehavior: 'none' } }} />
         <App />
       </ErrorBoundary>
     </ThemeUIProvider>
